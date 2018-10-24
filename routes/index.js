7403d8bccd8fc4bc82c41f536b8ba954aea2cae3
Antonio Bares <antonio.g.bares@gmail.com>
2018-10-23 17:35:41 -0700

    Added boilerplate code for routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* Load other routes */
 router.use('/sign-in', require('./sign-in'));
 router.use('/sign-up', require('./sign-up'));
+router.use('/dashboard', require('./dashboard'));
+router.use('/dashboard/settings', require('./settings'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
