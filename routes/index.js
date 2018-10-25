8b2a152d0b4e291690013f7a05a3de05f191ab9f
Antonio Bares <antonio.g.bares@gmail.com>
2018-10-25 14:52:48 -0700

    Initializing passport strategy

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,28 @@
+/**
+ * Main route that loads other routes.
+ * @module index
+ */
+
+/* Dependencies */
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var initPassport = require('../middlewares/passport').initPassport;
+
+/* Initialize Passport */
+initPassport();
 
 /* Load other routes */
 router.use('/sign-in', require('./sign-in'));
 router.use('/sign-up', require('./sign-up'));
+router.use('/sign-out', require('./sign-out'));
 router.use('/dashboard', require('./dashboard'));
 router.use('/dashboard/settings', require('./settings'));
 
-/* GET home page. */
+/* Routes */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Spartan Financial Group (SFG) Banking' });
 });
 
+/* Export Module */
 module.exports = router;
