bdb92e82828e56e3199510e6373b28018b40c01f
Antonio Bares <antonio.g.bares@gmail.com>
2018-11-01 10:31:24 -0700

    Added new Account route

---
+++ routes/account.js
@@ -0,0 +1,27 @@
+/**
+ * account route that defines account logic.
+ * @module account
+ */
+
+/* Dependencies */
+var express = require('express');
+var router = express.Router();
+var auth = require('../middlewares/auth');
+var Account = require('../models/account');
+
+/* Routes */
+router.get('/:accountId', auth.isAuthenticated, function(req, res, next) {
+  
+  // res.render();
+  res.send('Specific Account Page'); // Remove when using res.render()
+});
+
+
+router.get('/:accountId/transfer', auth.isAuthenticated, function(req, res, next) {
+  
+  // res.render();
+  res.send('Account Transfer Page'); // Remove when using res.render()
+});
+
+/* Export Module */
+module.exports = router;
