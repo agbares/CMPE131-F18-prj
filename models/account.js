2eb8871303265beab83cf24ee45ad5fab85c923d
Antonio Bares <antonio.g.bares@gmail.com>
2018-11-01 17:06:12 -0700

    Removed unneeded dependencies

--- models/account.js
+++ models/account.js
@@ -4,10 +4,6 @@
  */
 
 /* Dependencies */
-const bcrypt = require('bcrypt');
-const ObjectID = require('mongodb').ObjectID;
-const db = require('../db');
-const constants = require('../db_config');
 const mongoose = require('mongoose');
 
 var accountSchema = mongoose.Schema({
