9f1f39aa854b8991a9d387efb0508210d83c8451
Jason Chong <jasonchong0226@gmail.com>
2018-11-17 14:02:09 -0800

    Cleaned up the transfer code. Everything has been tested and works.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -22,7 +22,7 @@ router.get('/', auth.isAuthenticated, function(req, res) {
     if(err){
       console.log(err);
     }
-    console.log(accounts);
+    //console.log(accounts);
     
     for(var i = 0; i < accounts.length; i++)
     {
@@ -39,7 +39,7 @@ router.get('/', auth.isAuthenticated, function(req, res) {
         console.log('No Accounts');
       }
     }
-    console.log(accountObj.checkingAccount);
+    // console.log(accountObj.checkingAccount);
     res.render('dashboard/index', accountObj);
   })
 });
@@ -70,8 +70,87 @@ router.get('/transfer', auth.isAuthenticated, function(req, res, next) {
     next(err);
   });
 });
+router.post('/transfer', function(req, res, next){
+  const transferAmount = req['body']['transferamount']; //Getting the amount from the user.
+  const transferFrom = req['body']['transferfrom']; //Getting the radio choice. 
+  const transferTo = req['body']['transferto']; //Getting the radio choice. 
 
+  console.log(transferAmount);
 
+  if(validAmount(transferAmount) == true){
+    transferMoney(transferAmount, transferFrom, transferTo, req.user._id);
+  }
+  else
+  {
+    req.flash('error', 'Amount is negative.');
+    return res.redirect('transfer');
+    // console.log('Amount is negative.');
+    // res.render('/transfer');
+  }
+  res.render('dashboard/transfer');
+})
+
+function transferMoney(temp_transfer_amount, temp_transfer_from, temp_transfer_to, user_ID){ //A function where the transferring will be happening. 
+  Account.find({user_ID: user_ID}, function(err, accounts){
+    if(err){
+      console.log(err);
+    }
+    console.log(accounts.length);
+    console.log(user_ID);
+    var tempAmountHold = 0;
+
+    if(temp_transfer_from == temp_transfer_to){
+      console.log("Can't transfer money in the same account."); //"Same Account" as in transferring from savings to savings, checkings to checkings, etc. 
+    }
+
+    
+
+    for(var i = 0; i < accounts.length; i++){
+      console.log(accounts[i].type);
+      console.log(temp_transfer_to);
+
+      if(accounts[i].type == temp_transfer_from){
+        for(var j = 0; j < accounts.length; j++){
+          if(accounts[j].type == temp_transfer_to){
+            accounts[i].balance -= temp_transfer_amount;
+            tempAmountHold = parseInt(accounts[j].balance) + parseInt(temp_transfer_amount);
+            accounts[j].balance = tempAmountHold;
+            var accountsChecking = accounts[i];
+            var accountsSaving = accounts[j];
+            accountsChecking.save(function(err, res){
+              if(err){
+                console.log(err);
+              }
+              console.log(res);
+            })
+
+            accountsSaving.save(function(err, res){
+              if(err){
+                console.log(err);
+              }
+              console.log(res);
+            })
+          }
+          else{
+            console.log('Not the correct account.');
+          }
+        }
+      }
+      else{
+        console.log('Not the correct account.');
+      }
+    }
+  })
+}
+
+function validAmount(amount){ //Helper function to see if the user put in a positive amount. 
+  if(amount > 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
 router.get('/billpay', auth.isAuthenticated, function(req, res, next) {
   var accountObj = {
